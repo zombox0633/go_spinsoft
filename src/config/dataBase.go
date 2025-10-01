1e24683d704f2594fe3ac36014fe8a958e5b58fd
zombox0633 <ghostzb@outlook.com>
2025-10-01 12:57:27 +0700

    add: log InitDatabase status

--- src/config/dataBase.go
+++ src/config/dataBase.go
@@ -22,7 +22,7 @@ func InitDatabase(ctx context.Context, cfg *ConfigType) error {
 	defer cancel()
 
 	if cfg.MonGoURL == "" {
-		return fmt.Errorf("MongoDB URL is empty - check your .env file")
+		return fmt.Errorf("MongoDB URL is empty : check your .env file")
 	}
 
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(cfg.MonGoURL))
@@ -48,8 +48,10 @@ func InitDatabase(ctx context.Context, cfg *ConfigType) error {
 
 func (d *DatabaseType) Close(ctx context.Context) error {
 	if d.Client != nil {
-		log.Panicln("Closing MongoDB connection...")
+		log.Println("Closing MongoDB connection...")
 		return d.Client.Disconnect(ctx)
 	}
+
+	log.Println("MongoDB connection closed successfully")
 	return nil
 }
