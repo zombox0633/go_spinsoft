fd1ff66dbd47769d46b5c356cd8b2443111825b5
zombox0633 <ghostzb@outlook.com>
2025-10-01 17:47:19 +0700

    add: NearestStationRequest and NearestStationResponse dto

--- src/station/dto.go
+++ src/station/dto.go
@@ -1,5 +1,6 @@
 package station
 
+// Station Import
 type StationImportRequest struct {
 	URL string `json:"url" validate:"required,url"`
 }
@@ -8,4 +9,19 @@ type StationImportResponse struct {
 	Success       bool   `json:"success"`
 	ImportedCount int    `json:"imported_count"`
 	Message       string `json:"message"`
+}
+
+// Find Near Station
+type NearestStationRequest struct {
+	Lat  float64 `json:"lat" validate:"required"`
+	Long float64 `json:"long" validate:"required"`
+}
+
+type NearestStationResponse struct {
+	ID       int     `json:"id"`
+	Name     string  `json:"name"`
+	EnName   string  `json:"en_name"`
+	Lat      float64 `json:"lat"`
+	Long     float64 `json:"long"`
+	Distance float64 `json:"distance"`
 }
